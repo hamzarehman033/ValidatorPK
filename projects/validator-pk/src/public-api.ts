6230779119895ca94b87bcca0206b7ecd215ebe0
Hamza <hamza.brij.it>
2024-11-30 21:35:47 +0500

    directive export

--- projects/validator-pk/src/public-api.ts
+++ projects/validator-pk/src/public-api.ts
@@ -4,4 +4,5 @@
 
 export * from './lib/validator-pk.service';
 export * from './lib/validator-pk.component';
+export * from './lib/validator-pk.directive';
 export * from './lib/validator-pk.module';
